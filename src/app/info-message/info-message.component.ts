475ed28e5f6f940254eaf0c7b25aec94a0c84c58
Sai <sailakshmy94@gmail.com>
2025-07-16 12:41:42 +0400

    Integrated the onPush strategy in other components

--- src/app/info-message/info-message.component.ts
+++ src/app/info-message/info-message.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: 'app-info-message',
+  selector: "app-info-message",
   standalone: true,
   imports: [],
-  templateUrl: './info-message.component.html',
-  styleUrl: './info-message.component.css',
+  templateUrl: "./info-message.component.html",
+  styleUrl: "./info-message.component.css",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoMessageComponent {
   get debugOutput() {
     console.log('[InfoMessages] "debugOutput" binding re-evaluated.');
-    return 'InfoMessage Component Debug Output';
+    return "InfoMessage Component Debug Output";
   }
 
   onLog() {
-    console.log('Clicked!');
+    console.log("Clicked!");
   }
 }
